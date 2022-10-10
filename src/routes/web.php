6f806931fefda21b0feffeb537d3886178e8b839
Rohit Suthar <rohisuthar@gmail.com>
2022-10-10 16:40:25 +0530

    Initial release

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('inspire', [Codetyme\Inspire\Controllers\InspirationController::class, '__invoke']);
+Route::get('inspire', [\Codetyme\Inspire\Controllers\InspirationController::class, '__invoke']);
 
 ?>
